fd61f674833eb81bf9324688aa000c986d61ffc0
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-19 12:14:12 +0100

    initial commit

---
+++ src/main/java/ifri/dev/musicstreamingbackend/MusicStreamingBackendApplication.java
@@ -0,0 +1,13 @@
+package ifri.dev.musicstreamingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStreamingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStreamingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifri/dev/musicstreamingbackend/MusicStreamingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ifri.dev.musicstreamingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
