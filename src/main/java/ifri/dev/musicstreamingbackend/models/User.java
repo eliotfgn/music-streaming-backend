c164e002a6be4ad1d9ba37c9ec68d260e5c983c1
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-20 22:19:10 +0100

    validators for user model

--- src/main/java/ifri/dev/musicstreamingbackend/models/User.java
+++ src/main/java/ifri/dev/musicstreamingbackend/models/User.java
@@ -3,6 +3,10 @@ package ifri.dev.musicstreamingbackend.models;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @AllArgsConstructor
@@ -14,8 +18,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Email is required")
+    @NotEmpty(message = "Email is required")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotNull(message = "Username is required")
+    @NotNull(message = "Username is required")
+    @NotEmpty(message = "Username is required")
     private String username;
+    @NotNull(message = "Password is required")
+    @NotEmpty(message = "Password is required")
+    @Size(min = 8, message = "Password should be at least 8 characters")
     private String password;
     private String firstname;
     private String lastname;
