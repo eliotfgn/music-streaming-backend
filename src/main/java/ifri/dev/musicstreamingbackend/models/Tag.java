b0388b645b0ea7fccbf426fb4f194bc1bb967c0a
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-20 12:14:15 +0100

    add tag model

---
+++ src/main/java/ifri/dev/musicstreamingbackend/models/Tag.java
@@ -0,0 +1,21 @@
+package ifri.dev.musicstreamingbackend.models;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
