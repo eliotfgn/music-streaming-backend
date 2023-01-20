8a2706eb0c421552aec2dae4bc07177d8c22f203
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-20 22:27:59 +0100

    validators for artist model

--- src/main/java/ifri/dev/musicstreamingbackend/models/Artist.java
+++ src/main/java/ifri/dev/musicstreamingbackend/models/Artist.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
@@ -18,6 +20,8 @@ public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull(message = "Artist should have a name")
+    @NotEmpty(message = "Artist name should not be empty")
     private String name;
     private String bio;
     private String profilePic;
