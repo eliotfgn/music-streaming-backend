e4d319e6aced5e4972ced308d96befd2ce5b4222
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-25 03:15:52 +0100

    fix new tag creation

--- src/main/java/ifri/dev/musicstreamingbackend/services/TagService.java
+++ src/main/java/ifri/dev/musicstreamingbackend/services/TagService.java
@@ -6,17 +6,25 @@ import ifri.dev.musicstreamingbackend.repositories.TagRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class TagService {
     private TagRepository tagRepository;
 
-    public void create(String tag) {
+    public Tag create(String tag) {
         Tag entity = new Tag();
-        if (tagRepository.existsByNameIgnoreCase(tag)) {
-            return;
+        Optional<Tag> optionalTag = findEntityByName(tag);
+        if (optionalTag.isPresent()) {
+            return optionalTag.get();
         }
         entity.setName(tag);
+        return tagRepository.save(entity);
+    }
+
+    public Optional<Tag> findEntityByName(String name) {
+        return tagRepository.findByNameIgnoreCase(name);
     }
 
     public String findByName(String name) {
