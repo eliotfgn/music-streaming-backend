57854eec14184cf8dc4fe6ec59da5cd490d21b27
Eliot Fagnon <efagnon2002@gmail.com>
2023-02-02 20:33:45 +0100

    add playlist dto

---
+++ src/main/java/ifri/dev/musicstreamingbackend/models/PlaylistDto.java
@@ -0,0 +1,24 @@
+package ifri.dev.musicstreamingbackend.models;
+
+import ifri.dev.musicstreamingbackend.dto.TrackDto;
+import ifri.dev.musicstreamingbackend.dto.UserDto;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * A DTO for the {@link Playlist} entity
+ */
+public record PlaylistDto(Long id,
+                          @NotBlank(message = "Playlist name is required") String name,
+                          String description,
+                          String cover,
+                          Integer likesCount,
+                          Boolean isPrivate,
+                          UserDto user,
+                          List<TrackDto> tracks,
+                          LocalDate createdAt,
+                          LocalDate updateAt) implements Serializable {
+}
